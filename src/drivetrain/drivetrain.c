591d9a00b51381b7ddd4a0df70bb9f5e73d5a5ca
Loren Snow <lorensnow@protonmail.com>
2026-05-11 14:51:12 -0700

    use clr_mask and set_mask; define ALL_INPUTS; set pin directions

--- src/drivetrain/drivetrain.c
+++ src/drivetrain/drivetrain.c
@@ -8,35 +8,34 @@
 #define INPUT3 0x10000  /// Physical pin 21 / GPIO pin 16
 #define INPUT4 0x20000  /// Physical pin 22 / GPIO pin 17
 
+#define ALL_INPUTS (INPUT1 | INPUT2 | INPUT3 | INPUT4)  /// All input pins
+
 void drivetrain_init(void) {
-    gpio_init_mask(INPUT1 | INPUT2 | INPUT3 | INPUT4);
+    gpio_init_mask(ALL_INPUTS);
+    gpio_set_dir_out_masked(ALL_INPUTS);
     drive_brake();
 }
 
 void drive_fwd(void) {
-    gpio_put_masked(INPUT1 | INPUT3, 1);
-    gpio_put_masked(INPUT2 | INPUT4, 0);
+    gpio_set_mask(INPUT1 | INPUT3);
+    gpio_clr_mask(INPUT2 | INPUT4);
 }
 
 void drive_reverse(void) {
-    gpio_put_masked(INPUT1 | INPUT3, 0);
-    gpio_put_masked(INPUT2 | INPUT4, 1);
+    gpio_clr_mask(INPUT1 | INPUT3);
+    gpio_set_mask(INPUT2 | INPUT4);
 }
 
-void drive_brake(void) {
-    gpio_put_masked(INPUT1 | INPUT2 | INPUT3 | INPUT4, 1);
-}
+void drive_brake(void) { gpio_set_mask(ALL_INPUTS); }
 
-void drive_coast(void) {
-    gpio_put_masked(INPUT1 | INPUT2 | INPUT3 | INPUT4, 0);
-}
+void drive_coast(void) { gpio_clr_mask(ALL_INPUTS); }
 
 void drive_left(void) {
-    gpio_put_masked(INPUT1 | INPUT4, 0);
-    gpio_put_masked(INPUT2 | INPUT3, 1);
+    gpio_set_mask(INPUT1 | INPUT4);
+    gpio_clr_mask(INPUT2 | INPUT3);
 }
 
 void drive_right(void) {
-    gpio_put_masked(INPUT1 | INPUT4, 1);
-    gpio_put_masked(INPUT2 | INPUT3, 0);
+    gpio_set_mask(INPUT1 | INPUT4);
+    gpio_clr_mask(INPUT2 | INPUT3);
 }
