c3c77a8812cda71b495e1d9861918befa459ab97
Loren Snow <lorensnow@protonmail.com>
2026-06-30 13:39:12 -0700

    add proximity sensor to robot

--- src/robot.c
+++ src/robot.c
@@ -1,13 +1,17 @@
-#include "btstack.h"
 #include "pico/cyw43_arch.h"
+#include "pico/multicore.h"
 #include "pico/printf.h"
-#include "pico/stdlib.h"
 
 #include "bluetooth/ble_client.h"
 #include "motor_control.h"
+#include "hcsr04.h"
 
 #define GREEN_LED_PIN 15  // GPIO 15
 #define RED_LED_PIN   14  // GPIO 14
+#define PROX_TRIG_PIN 27
+#define PROX_ECHO_PIN 28
+
+void core1_routine(void);
 
 int main(void) {
     indicator_pins_t led_pins = {
@@ -16,6 +20,8 @@ int main(void) {
     };
 
     stdio_init_all();
+
+    multicore_launch_core1(core1_routine);
 
     motor_control_init();
 
@@ -26,4 +32,14 @@ int main(void) {
     btstack_run_loop_execute();
 
     return 0;
+}
+
+void core1_routine(void) {
+    hcsr04_init(PROX_TRIG_PIN, PROX_ECHO_PIN);
+
+    while (1) {
+        double dist = hcsr04_get_distance(MM);
+
+        printf("Distance: %.2f\n", dist);
+    }
 }
